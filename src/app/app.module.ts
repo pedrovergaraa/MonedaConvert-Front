e5ad3aad69207415712551c6694f1321c835d5e1
pedrovergaraa <pvergara2407@gmail.com>
2024-09-27 12:55:36 -0300

    Estilizando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ErrorComponent } from './components/errors/error.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ErrorMessageComponent } from './components/error-message/error-message.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ErrorComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ErrorMessageComponent
   ],
   imports: [
     BrowserModule,
